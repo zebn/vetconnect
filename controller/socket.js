60ec42e385c42082e212646514b3cd0a4b7a1c7a
zebn <zebn@bk.ru>
2024-06-07 14:17:12 +0200

    fixed socket

--- controller/socket.js
+++ controller/socket.js
@@ -76,7 +76,7 @@ class SocketController {
                     let response = await chatbot.generateResponseAI(data.message);
                     console.log(response)
                     let databotanswer = {
-                        message: response.answer !== undefined ? response.answer : "Lo siento, no entiendo :(",
+                        message: response.answer !== undefined ? response.answer : "Lo siento, no entiendo. Necesito esperar al veterinario.",
                         roomId: data.roomId,
                         username: "Bot@bot.com",
                         file: null,
